2e70705bb1d8a70c666c9c2781ccfb993e6a614b
joao <joaogalleni1376@gmail.com>
2026-03-19 19:37:40 -0300

    correcao e adição do campo fieldCondition

--- src/components/Area.jsx
+++ src/components/Area.jsx
@@ -12,7 +12,7 @@ export default function Area({eletronicos, nomeArea}){
                     <div className="price-name-condition">
                         <h3>{element.name}</h3>
                         <h4>{element.condition}</h4>
-                        <h4 className="elementPrice">{element.price}</h4>
+                        <h4 className="elementPrice">{element.preco}</h4>
                     </div>
                 </div>)}
             </div>
---
+++ src/components/FieldCondition.jsx
@@ -0,0 +1,11 @@
+export default function FieldCondition({value, onChange}){
+
+    return(
+        <>
+        <label htmlFor="usado"> usado </label>
+        <input type="radio" name="condition" id="usado" value="usado" checked={value == "usado"} onChange={onChange} /><br/>
+        <label htmlFor="novo"> novo </label>
+        <input type="radio" name="condition" id="novo" value="novo" checked={value == "novo"} onChange={onChange}/><br/>
+        </>
+    );
+}
